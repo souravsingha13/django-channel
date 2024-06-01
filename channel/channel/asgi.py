a243fd0ee67b525d19519d53526e687666114a98
souravsingha13 <souravsingha1304021@gmail.com>
2024-06-02 00:42:50 +0600

    createc consummer

--- channel/channel/asgi.py
+++ channel/channel/asgi.py
@@ -1,22 +1,20 @@
-"""
-ASGI config for channel project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'channel.settings')
+
+application = get_asgi_application()
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
-        # Just HTTP for now. (We can add other protocols later.)
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
     }
 )
--- channel/channel/settings.py
+++ channel/channel/settings.py
@@ -72,7 +72,16 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "channel.wsgi.application"
-ASGI_APPLICATION = "channel.asgi.application"
+ASGI_APPLICATION = 'channel.asgi.application'
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
